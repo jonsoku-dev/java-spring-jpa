1237bf119e0232c3ed8068d0377ea63c3d468df4
jonsoku2 <whdtjr2792@gmail.com>
2020-12-16 11:53:08 +0900

    spring jpa를 사용하여 Auditing createdBy, updatedBy

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,7 +2,12 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @EnableJpaAuditing // 반드시 넣어야함 !!!! Auditing 할 때
 @SpringBootApplication
@@ -10,6 +15,18 @@ public class DataJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataJpaApplication.class, args);
+    }
+
+    @Bean // 반드시 넣어야함 !!!! Auditing createdBy 할 때
+    public AuditorAware<String> auditorProvider() {
+        // 람다
+        return () -> Optional.of(UUID.randomUUID().toString());
+//        return new AuditorAware<String>() {
+//            @Override
+//            public Optional<String> getCurrentAuditor() {
+//                return Optional.of(UUID.randomUUID().toString());
+//            }
+//        };
     }
 
 }
--- src/main/java/study/datajpa/entity/BaseEntity.java
+++ src/main/java/study/datajpa/entity/BaseEntity.java
@@ -1,7 +1,9 @@
 package study.datajpa.entity;
 
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -10,7 +12,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(AuditingEntityListener.class) // DataJpaApplication 확인
 @MappedSuperclass
 @Getter
 public class BaseEntity {
@@ -21,4 +23,11 @@ public class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy; // DataJpaApplication 확인
 }
